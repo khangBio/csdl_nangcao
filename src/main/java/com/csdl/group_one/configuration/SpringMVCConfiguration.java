9f02a041598dabfbd968f398230f91f22f96cb21
khangtn3894 <khangtn3894@gmail.com>
2025-10-16 22:06:57 +0700

    config database

--- src/main/java/com/csdl/group_one/configuration/SpringMVCConfiguration.java
+++ src/main/java/com/csdl/group_one/configuration/SpringMVCConfiguration.java
@@ -1,12 +1,39 @@
 package com.csdl.group_one.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringMVCConfiguration {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() throws IllegalArgumentException, NamingException {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        ds.setUrl(env.getProperty("spring.datasource.url"));
+        ds.setUsername(env.getProperty("spring.datasource.username"));
+        ds.setPassword(env.getProperty("spring.datasource.password"));
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
 //    @Bean
 //    public TilesConfigurer tilesConfigurer() {
 //        TilesConfigurer tilesConfigurer = new TilesConfigurer();
@@ -21,4 +48,5 @@ public class SpringMVCConfiguration {
 //        viewResolver.setViewClass(TilesView.class);
 //        return viewResolver;
 //    }
+
 }
