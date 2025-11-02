830453b97e074f3d5d4666a3b24581115bde4db3
DESKTOP-3LR7SAT\Admin <khiemtv.tgg>
2025-11-02 23:32:09 +0700

    commit

--- src/main/java/com/csdl/group_one/repository/DanhMucSanPhamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csdl.group_one.repository;
-
-import com.csdl.group_one.model.DanhMucSanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DanhMucSanPhamRepository extends JpaRepository<DanhMucSanPham, Integer> {
-}
--- src/main/java/com/csdl/group_one/repository/KhachHangRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csdl.group_one.repository;
-
-import com.csdl.group_one.model.KhachHang;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KhachHangRepository extends JpaRepository<KhachHang, Integer> {
-}
